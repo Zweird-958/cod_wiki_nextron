6d3ff55829e5a1bfd0abb27052107dfad0f4a73a
Zweird-958 <xdfamilyytt@gmail.com>
2023-02-03 13:35:18 +0100

    add onClick

--- renderer/components/TextWithImageBelow.jsx
+++ renderer/components/TextWithImageBelow.jsx
@@ -2,18 +2,17 @@ import GameScreen from "./GameScreen"
 import ItemsDiv from "./ItemsDiv"
 
 const TextWithImageBelow = (props) => {
-  const { children, items } = props
+  const { children, items, onClick } = props
 
   return (
     <>
       <p className="mx-auto my-5 max-w-3xl text-center">{children}</p>
       <ItemsDiv className="gap-4">
         {items.map((item, index) => (
-          <GameScreen key={index} src={item.src} alt={item.alt} />
+          <button onClick={onClick} key={index}>
+            <GameScreen src={item.src} alt={item.alt} />
+          </button>
         ))}
-        {/* <GameScreen src={`${easterEggImages}step1/fourth.png`} alt="test" />
-        <GameScreen src={`${easterEggImages}step1/fifth.png`} alt="test" />
-        <GameScreen src={`${easterEggImages}step1/sixth.png`} alt="test" /> */}
       </ItemsDiv>
     </>
   )
