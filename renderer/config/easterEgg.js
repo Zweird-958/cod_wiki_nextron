5d7b42ebd2ddd3aabf6d50c01a44059d120820fb
Zweird-958 <xdfamilyytt@gmail.com>
2023-02-05 23:03:36 +0100

    Create easterEgg.js

---
+++ renderer/config/easterEgg.js
@@ -0,0 +1,21 @@
+import DownloadFile from "../components/DownloadFile"
+
+const MAPS = {
+  test: [
+    { text: "Tout d'abord vous devrez..." },
+    {
+      images: ["step1/first.png"],
+      text: "hlesjlkfcholshfoshgdcgslfgs",
+    },
+    {
+      images: ["step1/first.png", "", ""],
+      text: (
+        <DownloadFile href="https://github.com/Kenshin9977/aotrt-chemicals/releases/download/1.0/AOTRT.-.Chemicals.FR.v1.0.exe">
+          programme
+        </DownloadFile>
+      ),
+    },
+  ],
+}
+
+export default MAPS
