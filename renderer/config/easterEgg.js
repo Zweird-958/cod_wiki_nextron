0a02aff011ec74ce18d35dc48cc77fe2a4104c25
Zweird-958 <xdfamilyytt@gmail.com>
2023-02-09 14:42:24 +0100

    deleted test map

--- renderer/config/easterEgg.js
+++ renderer/config/easterEgg.js
@@ -1,21 +1,6 @@
 import DownloadFile from "../components/DownloadFile"
 
 const MAPS = {
-  test: [
-    { text: "Tout d'abord vous devrez..." },
-    {
-      images: ["step1/first.png"],
-      text: "hlesjlkfcholshfoshgdcgslfgs",
-    },
-    {
-      images: ["step1/first.png", "", ""],
-      text: (
-        <DownloadFile href="https://github.com/Kenshin9977/aotrt-chemicals/releases/download/1.0/AOTRT.-.Chemicals.FR.v1.0.exe">
-          programme
-        </DownloadFile>
-      ),
-    },
-  ],
   spaceland: [
     {
       text: "Le secret est faisable de 1 à 4 joueurs, vous aurez besoin du Pack à Punch et d’au moins une arme spéciale (une par joueur est recommandé).",
