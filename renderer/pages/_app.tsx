977a630059f2bd91bbde16a91e4980447a946d9f
Zweird-958 <xdfamilyytt@gmail.com>
2023-02-12 22:31:17 +0100

    title on icon

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -13,7 +13,7 @@ const NAV_ITEMS = {
   SLEEDGEHAMER: "/studios/sleedgehammer",
 }
 const Icon = (props) => {
-  const { icon, onClick, ...otherProps } = props
+  const { icon, onClick, title, ...otherProps } = props
 
   return (
     <svg
@@ -26,6 +26,7 @@ const Icon = (props) => {
       className="mx-1 h-6 w-6 cursor-pointer hover:text-blue-800"
       {...otherProps}
     >
+      <title>{title}</title>
       <path strokeLinecap="round" strokeLinejoin="round" d={icon} />
     </svg>
   )
@@ -71,22 +72,32 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
             <Icon
               icon="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
               onClick={handleBack(router)}
+              title="Back"
             />
             <div id="drag" className="w-full">
               <h2 className="ml-5 font-medium text-gray-400">ZOMBIES WIKI</h2>
             </div>
           </div>
           <div className="flex">
-            <Icon onClick={minimizeButton} icon="M19.5 12h-15" />
+            <Icon
+              onClick={minimizeButton}
+              icon="M19.5 12h-15"
+              title="Minimize"
+            />
             <Icon
               onClick={maximizeButton}
+              title={maximize ? "Minimize" : "Maximize"}
               icon={
                 maximize
                   ? "M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25"
                   : "M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
               }
             />
-            <Icon onClick={closeButton} icon="M6 18L18 6M6 6l12 12" />
+            <Icon
+              onClick={closeButton}
+              title="Close"
+              icon="M6 18L18 6M6 6l12 12"
+            />
           </div>
         </div>
         <div className="bg-dark-blue flex justify-between p-5 ">
